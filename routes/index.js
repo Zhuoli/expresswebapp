f28acf5a9f0c8fa108fe1e6922ea048393b84230
zhuoli@zhuoliliang.com <zhuoli@zhuoliliang.com>
2017-07-02 19:59:51 -0700

    update home page content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'StockAssistant' });
+  res.render('index', { title: 'Web Home page' });
 });
 
 module.exports = router;
